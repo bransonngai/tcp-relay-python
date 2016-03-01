c9bac911b4c49b3e4c30d8aa6d41c9566482105e
Mikhail Strizhov <strizhov@copper.cs.colostate.edu>
2016-03-01 14:21:09 -0700

    source code

---
+++ echoserver.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# Author: Mikhail Strizhov
+# Date: Feb 27, 2016
+
+import sys
+import socket
+
+def usage():
+    print "Usage: ./echoserver [relay host] [relay port]"
+    sys.exit(1) 
+
+if __name__=='__main__':
+    host = ''
+    port = ''
+    size = 4096
+
+    # Check command line arguments
+    total = len(sys.argv)
+    if total < 3:
+        usage()
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    
+    if not (host or port):
+        usage()
+
+    # Connect to the relay server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+
+    # Receive a tunnel host & port information
+    data = s.recv(size)
+
+    print data
+
+    # Now receive any packets from the relay server and send back
+    running  = 1
+    while running:
+        data = s.recv(size)
+        if data:
+            s.send(data)
+        else:
+            running = 0
+
+    s.close()
